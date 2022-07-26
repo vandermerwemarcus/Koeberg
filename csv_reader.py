8c7f1937367f65c7cb4880654c92993bafaaa062
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 20:49:40 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
+     #st.write(bytes_data)
+     df=pd.read_csv(uploaded_file)
+     print(df)

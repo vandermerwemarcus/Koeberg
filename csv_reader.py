5cf18446f99320f731b83369eb64d97e296bfca1
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 21:54:14 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-st.title('GPV VALVE TIMES READER')
+st.header('GPV VALVE TIMES READER')
+gv=pd.DataFrame()
+gv['G1']=0
 uploaded_file = st.file_uploader("Upload csv file here:")
 if uploaded_file is not None:
      df=pd.read_csv(uploaded_file,low_memory=False)
-     
+     for i in range(9,len(df),100):
+          gv.loc[i,'gv']=df.iloc[i,1]
      fig,ax = plt.subplots(figsize=[12,8])
-     ax.plot(df.iloc[9:,1],label='GV1')
-     ax.plot(df.iloc[9:,2],label='GV2')
+     ax.plot(gv.iloc[:,:],label='GV1')
      ax.legend()
      ax.set_title('VALVE TIMES')
      #ax.set_xlabel('x label')

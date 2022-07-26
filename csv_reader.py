7d2bdd22aa04d188ae1c5486d507aa359c0dc210
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 21:28:46 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -9,19 +9,14 @@ if uploaded_file is not None:
      df=pd.read_csv(uploaded_file,low_memory=False)
      st.write(df)
      #st.line_chart(df.iloc[9:100,1],width=10, height=10)
-     fig=plt.figure(figsize=(10,6),tight_layout=True,dpi=100)#, dpi=200) #Allows the figure size to be specified
-     ax1=fig.add_subplot(3,1,1)
-     plt.grid(which='both', axis='both')
-     plt.yscale('log')
-     ax2=fig.add_subplot(3,1,2,sharex=ax1) #sharex aligns the x-axis'
-     plt.grid(which='both', axis='both')
-     ax3=fig.add_subplot(3,1,3,sharex=ax1)
-     plt.grid(which='both', axis='both')
-     ax1.plot(df.iloc[9:100,1])
-     #plt.yscale('log')
-     ax2.plot(df.iloc[9:100,1])
-     #ax3.plot(UZ.loc[:,'MACD'])
-     ax1.set_title('Security')
-     ax2.set_title('ROC=10-50')
-     ax3.set_title('ROC=2-8')
-     plt.show()
+     
+     fig,ax = plt.subplots(figsize=[10,5])
+     y=df.iloc[9:100,1]
+     ax.plot(y,label='GV1')
+
+     ax.legend()
+     ax.set_title(tic)
+     #ax.set_xlabel('x label')
+     ax.set_ylabel('Price')
+     ax.grid(True)
+     st.pyplot(fig)

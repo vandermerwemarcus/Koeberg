366aae51f03bc35e03f8bebc9eb0a7fb0541d5c7
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 21:38:55 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -7,16 +7,14 @@ if uploaded_file is not None:
      #bytes_data = uploaded_file.getvalue()
      #st.write(bytes_data)
      df=pd.read_csv(uploaded_file,low_memory=False)
-     st.write(df)
-     #st.line_chart(df.iloc[9:,1],width=10, height=10)
      
-     fig,ax = plt.subplots(figsize=[10,5])
+     fig,ax = plt.subplots(figsize=[12,8])
      y=df.iloc[9:,1]
      ax.plot(y,label='GV1')
 
      ax.legend()
      ax.set_title('VALVE TIMES')
      #ax.set_xlabel('x label')
-     ax.set_ylabel('Price')
-     ax.grid(True)
+     ax.set_ylabel('Travel')
+     #ax.grid(True)
      st.pyplot(fig)

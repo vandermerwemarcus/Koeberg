782ad30ed10d6e3a4bce98f390a67ad93deca8ce
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 22:08:14 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -8,7 +8,7 @@ uploaded_file = st.file_uploader("Upload csv file here:")
 if uploaded_file is not None:
      df=pd.read_csv(uploaded_file,low_memory=False)
      for i in range(9,len(df),100):
-          gv.loc[i,'G1']=df.iloc[i,1]
+          gv.loc[i,'G1']=df.iloc[len(df)-i,1]
      st.write(gv.loc[:,:])
      fig,ax = plt.subplots(figsize=[12,8])
      ax.plot(gv.loc[:,'G1'],label='GV1')

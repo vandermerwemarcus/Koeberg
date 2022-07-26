a405d5113a90f3f3b1f6668217696291b6a36c18
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 21:58:10 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -8,10 +8,10 @@ uploaded_file = st.file_uploader("Upload csv file here:")
 if uploaded_file is not None:
      df=pd.read_csv(uploaded_file,low_memory=False)
      for i in range(9,len(df),100):
-          gv.loc[i,'gv']=df.iloc[i,1]
+          gv.loc[i,'G1']=df.iloc[i,1]
      st.write(gv.loc[:,:])
      fig,ax = plt.subplots(figsize=[12,8])
-     ax.plot(gv.iloc[:,:],label='GV1')
+     ax.plot(gv.loc[:,'G1'],label='GV1')
      ax.legend()
      ax.set_title('VALVE TIMES')
      #ax.set_xlabel('x label')

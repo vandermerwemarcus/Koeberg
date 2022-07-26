b08a2d4ae72beb49c18da0a85ad5d5befd13f7f1
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 20:43:10 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,4 @@
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-     # To read file as bytes:
      bytes_data = uploaded_file.getvalue()
      st.write(bytes_data)

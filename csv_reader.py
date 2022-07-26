5ecdfa5ea9e298c9b57ed7794f2aba05347f3527
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 21:00:50 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -4,5 +4,5 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      bytes_data = uploaded_file.getvalue()
      #st.write(bytes_data)
-     df=pd.read_csv(uploaded_file)
+     df=pd.read_csv(uploaded_file,low_memory=False)
      st.write(df)

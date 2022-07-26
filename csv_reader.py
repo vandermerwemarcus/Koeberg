30509751c162661d28702c5d0469833a40e3548d
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 21:10:37 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-     bytes_data = uploaded_file.getvalue()
+     #bytes_data = uploaded_file.getvalue()
      #st.write(bytes_data)
      df=pd.read_csv(uploaded_file,low_memory=False)
      st.write(df)
-     st.line_chart(df.iloc[9:,1],width=10, height=10)
+     st.line_chart(df.iloc[9:,100],width=10, height=10)

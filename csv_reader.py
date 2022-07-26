5601818a6db496bab09526bab7bd638402faa384
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-26 22:13:26 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 st.header('GPV VALVE TIMES READER')
 gv=pd.DataFrame()
 gv['G1']=0
+
+#Upload csv data:
 uploaded_file = st.file_uploader("Upload csv file here:")
 if uploaded_file is not None:
      df=pd.read_csv(uploaded_file,low_memory=False)
      for i in range(9,len(df),100):
           gv.loc[i,'G1']=df.iloc[i,1]
      st.write(gv.loc[:,:])
+     
+     #Plot data using Matplotlib:
      fig,ax = plt.subplots(figsize=[12,8])
-     ax.plot(gv.loc[:,'G1'],label='GV1')
+     ax.plot(gv,label='GV1')
      ax.legend()
      ax.set_title('VALVE TIMES')
      #ax.set_xlabel('x label')
      ax.set_ylabel('Travel')
      #ax.grid(True)
      st.pyplot(fig)
-     st.line_chart(gv)

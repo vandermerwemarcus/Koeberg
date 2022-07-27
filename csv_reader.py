5db9f5c66362d2083fd6a0c1adfca42a65276ccd
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-27 11:41:34 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -13,7 +13,8 @@ if uploaded_file is not None:
      samplerate=float(df.iloc[6,1])
      st.write('Sample Rate:',samplerate)
      length=blocksize*samplerate
-     st.write('Length (sec):',length,round=2)
+     round(length,2)
+     st.write('Length (sec):',length)
      for i in range(10,len(df),50):
           gv.loc[i,'G1']=float(df.iloc[i,1])
      #st.write(gv.loc[:,:])

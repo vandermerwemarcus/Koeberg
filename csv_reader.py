f6e07571a2d58e9d05c179a70e3e3d5490c8b1ee
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-27 08:49:00 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -13,12 +13,13 @@ if uploaded_file is not None:
      st.write(gv.loc[:,:])
      
      #Plot data using Matplotlib:
-     fig,ax = plt.subplots(figsize=[12,8])
-     ax.plot(gv.loc[9:,'G1'])#,label='GV1')
+     fig,ax = plt.subplots(figsize=[12,6])
+     ax.plot(gv.loc[9:,'G1'],label='GV1')
      ax.legend()
      ax.set_title('VALVE TIMES')
      #ax.set_xlabel('x label')
      #ax.set_yticks([0,10])
+     ax.set_xticks([0,5,10])
      ax.set_yticklabels(['10','1'])
      ax.set_ylabel('Travel')
      #ax.grid(True)

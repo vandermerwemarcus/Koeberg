08ad8070d5b0cce203348876286f99f7a8c4f3b9
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-30 18:18:53 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -10,6 +10,7 @@ uploaded_file = st.file_uploader("Upload csv file here:")
 if uploaded_file is not None:
      @st.cache(suppress_st_warning=True,allow_output_mutation=True)
      def upload():
+          global df
           df=pd.read_csv(uploaded_file,low_memory=False)
      upload()
      st.write('Uploaded',df)

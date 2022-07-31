2aba46fbb6afaca6bbee2f6ea3a9d5867310e62b
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-07-31 17:02:40 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -8,7 +8,7 @@ gv=pd.DataFrame()
 df=pd.DataFrame()
 gv['G1']=0
 
-@st.experimental_memo#(suppress_st_warning=True)
+#@st.experimental_memo(suppress_st_warning=True)
 def upload():
      st.write('Ready csv upload')
      uploaded_file = st.file_uploader("Upload csv file here:")
